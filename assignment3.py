1b1895c3aca1bc163f7e09e504585021212bbd33
Tejas Sateesh <ts4044@nyu.edu>
2021-11-13 22:33:41 -0500

    Final

--- assignment3.py
+++ assignment3.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math
 
+
 ### Assignment 3 ###
 
 class KNN:
@@ -8,41 +9,31 @@ class KNN:
 		# KNN state here
 		# Feel free to add methods
 		self.k = k
+		self.instances = None
+		self.labels = None
 
-	def distance(self, featureA, featureB):
-		diffs = (featureA - featureB) ** 2
+	def distance(self, feature_a, feature_b):
+		diffs = (feature_a - feature_b) ** 2
 		return np.sqrt(diffs.sum())
 
 	def train(self, X, y):
 		# training logic here
 		# input is an array of features and labels
-		self.features = X
+		self.instances = X
 		self.labels = y
 
 	def predict(self, X):
 		# Run model here
 		predictions = []
 
-		for predict in X:
-			# Find the Euclidian distance with each of the rows in features and form an array
-			neighbours = [self.distance(predict, row) for row in self.features]
+		for instance in X:
+			distance_to_neighbours = [self.distance(instance, row) for row in self.instances]
+			k_closest_neighbours = np.argsort(distance_to_neighbours)[:self.k]
+			closest_neighbours_labels = [self.labels[i] for i in k_closest_neighbours]
+			neighbour_label, count = np.unique(closest_neighbours_labels, return_counts=True)
+			index_of_most_common_neighbour = np.argmax(count)
+			predictions.append(neighbour_label[index_of_most_common_neighbour])
 
-			# Sort the array and obtain the index of the top 'k' neighbours
-			k_sorted_neighbours = np.argsort(neighbours)[:self.k]
-
-			# Based on the indexes of the top k neighbours, get their labels
-			k_neighbours_labels = [self.labels[i] for i in k_sorted_neighbours]
-
-			# Count the labels
-			k_nearest_neighbours, count = np.unique(k_neighbours_labels, return_counts=True)
-
-			# Get the index of the label that has the maximum count
-			index_of_mode = np.argmax(count)
-
-			# Add the predicted label to the result list
-			predictions.append(k_nearest_neighbours[index_of_mode])
-
-		# Return array of predictions where there is one prediction for each set of features
 		return np.array(predictions)
 
 
@@ -58,44 +49,32 @@ class Perceptron:
 		# training logic here
 		# input is array of features and labels
 
-		# Get the size of training set, used for restarting the iteration over the dataset when the end is reached
 		training_size = len(X)
 
-		for s in range(steps):
+		for step in range(steps):
 			# Get the index of the current data point we are processing
-			index = s % training_size
-			data_point = X[index]
+			index = step % training_size
+			instance = X[index]
 
-			# Apply the activation function for the current data set
-			activation_output = np.dot(self.w, data_point) + self.b
-
-			# Apply the step function for the current data set
+			activation_output = np.dot(self.w, instance) + self.b
 			step_function_output = 1 if activation_output > 0 else 0
 
-			# If the output of the step function is not the same as label, update the weights
+			# If the prediction is mis-labeled, run the perceptron
 			if step_function_output != y[index]:
-				# Update the weights by finding the delta
-				# delta = learning rate * difference of actual and predicted label * x_i
-				# Leveraging the numpy array operations here to update the weights array
-				self.w = self.w + (self.lr * (y[index] - step_function_output) * data_point)
+				# w ← w + (η di xi)
+				self.w = self.w + (self.lr * (y[index] - step_function_output) * instance)
 
 	def predict(self, X):
 		# Run model here
 		# Return array of predictions where there is one prediction for each set of features
 
-		# Array for storing the prediction
 		result = []
 
-		# For each data in test set, do
 		for x in X:
-			# Find the output by running the activation function and then the step function
-			activation_output = np.dot(self.w, x) + self.b
-			step_function_output = 1 if activation_output > 0 else 0
-
-			# Append the prediction to result set
+			activation_function_output = np.dot(self.w, x) + self.b
+			step_function_output = 1 if activation_function_output > 0 else 0
 			result.append(step_function_output)
 
-		# Return the result set as a numpy array for evaluation
 		return np.array(result)
 
 
@@ -113,60 +92,89 @@ class ID3:
 		categorical_data = np.floor(self.bin_size * norm_data).astype(int)
 		return categorical_data
 
+	# This method calculates the Shannon Entropy for a given set of labels.
+	# For Shannon Entropy, we consider only the bins of the labels, passing the label list here suffices.
 	def calculate_entropy(self, labels):
 		total_count = len(labels)
 		unique_labels, counts = np.unique(labels, return_counts=True)
-		entropy = sum([(-(c / total_count) * math.log2(c / total_count)) if c > 0 else 0 for c in counts])
+		entropy = sum((-(c / total_count) * math.log2(c / total_count)) if c > 0 else 0 for c in counts)
 		return entropy
 
-	def calculate_information_gain(self, features, column, labels):
-		column_values = [features[i][column] for i in range(len(features))]
+	# This method returns the info_gain of a column for a given feature list.
+	# Bin the labels corresponding to unique feature bins for a given column, calculate the entropy and info gain.
+	def calculate_information_gain(self, instances, column, labels):
+		feature = [instance[column] for instance in instances]
 
-		unique_bins, counts = np.unique(column_values, return_counts=True)
+		unique_bins, counts = np.unique(feature, return_counts=True)
 		total_count = sum(counts)
 
 		bin_entropies = []
 		for x in unique_bins:
-			indexes = np.where(column_values == x)[0]
+			indexes = np.where(feature == x)[0]
 			corresponding_labels = [labels[i] for i in indexes]
 			bin_entropy = self.calculate_entropy(corresponding_labels)
 			bin_entropies.append(bin_entropy)
 
-		info_gain = sum([((counts[i] / total_count) * bin_entropies[i]) for i in range(len(bin_entropies))])
+		info_gain = sum(((counts[i] / total_count) * bin_entropies[i]) for i in range(len(bin_entropies)))
 		return info_gain
 
-	def get_best_gain_column(self, features, labels, columns_to_process):
+	# This method returns the column index of the feature with best gain.
+	# Calculate the gain for every column that has not already been considered for tree split, sort them
+	# Return the column index of the highest gain
+	def get_best_gain_column(self, instances, labels, columns_to_process):
 		total_entropy = self.calculate_entropy(labels)
 		gain = {}
 		for column in columns_to_process:
-			gain[column] = total_entropy - self.calculate_information_gain(features, column, labels)
+			gain[column] = total_entropy - self.calculate_information_gain(instances, column, labels)
 		return max(gain, key=gain.get)
 
+	# This method returns the most common value for a given array
 	def get_mode(self, values):
 		bins, counts = np.unique(values, return_counts=True)
 		return bins[np.argmax(counts)]
 
-	def decision_tree_learning(self, features, labels, columns_to_process, parent_examples=None):
-		if len(features) == 0:
+	# This is the main function of the algorithm training.
+	# We recursively calculate the nodes and append to tree
+	# Features is the set of datapoints that we are processing, labels are their corresponding subset from the y
+	# columns_to_process maintains the columns that have not yet been visited
+	# parent_examples holds the values of the previously selected best column from the parent subset
+	def decision_tree_learning(self, instances, labels, columns_to_process, parent_examples=None):
+		# If the examples is empty, return the PLURALITY_VALUES(parent_examples)
+		# Parent_Examples are values of the best gain column on which we have split
+		if len(instances) == 0:
 			return self.get_mode(parent_examples)
+		# Else If All examples have the same classification, return the classification
 		elif len(np.unique(labels)) == 1:
 			return labels[0]
 
 		# Else
-		best_column = self.get_best_gain_column(features, labels, columns_to_process)
+		# A ← argmax(a ∈ attributes) IMPORTANCE(a, examples)
+		best_column = self.get_best_gain_column(instances, labels, columns_to_process)
+		# Remove the best column from columns to process, since it is already selected
 		columns_to_process.remove(best_column)
+
+		# Tree will be constructed using a dictionary
+		# If the value of a key is a dictionary, then the key is a column number
+		# If the value of a key is an integer/string, then the key is the value on which we need to split
 		tree = {best_column: {}}
 
-		best_column_values = [feature[best_column] for feature in features]
+		best_column_values = [instance[best_column] for instance in instances]
+
+		# Calculate the exs ← {e : e ∈ examples and e.A = vk} - Bins of unique values of the selected column
 		bins = np.unique(best_column_values)
 
+		# If all the examples are same - return the PLURALITY_VALUE(classification) - most common label for the features
 		if len(bins) == 1:
 			return self.get_mode(labels)
 
 		for bin_value in bins:
-			feature_subset = features[best_column_values == bin_value]
+			# Create the subset of features for each bin. Obtain the corresponding labels.
+			instance_subset = instances[best_column_values == bin_value]
 			label_subset = labels[best_column_values == bin_value]
-			subtree = self.decision_tree_learning(feature_subset, label_subset, columns_to_process, best_column_values)
+
+			# subtree ← DECISION-TREE-LEARNING(exs, attributes − A, examples)
+			subtree = self.decision_tree_learning(instance_subset, label_subset, columns_to_process, best_column_values)
+			# Add a branch to tree with label (A = vk) and subtree subtree
 			tree[best_column][bin_value] = subtree
 
 		return tree
@@ -175,8 +183,8 @@ class ID3:
 		# training logic here
 		# input is array of features and labels
 		categorical_data = self.preprocess(X)
-		self.tree = self.decision_tree_learning(categorical_data, y, list(range(len(categorical_data[0]))))
-		print(self.tree)
+
+		self.tree = self.decision_tree_learning(categorical_data, y, list(range(categorical_data.shape[1])))
 
 	def predict(self, X):
 		# Run model here
@@ -186,16 +194,30 @@ class ID3:
 		predictions = []
 
 		for data in categorical_data:
-			model = self.tree
+			# Get the root node of the built decision tree
+			# Since I have used a dictionary, feeding in the entire dictionary as root node
+			root_node = self.tree
+
+			# Iteratively traverse through the nested dictionary to obtain the prediction
+			# While a leaf has not been reached, do
 			while 1:
-				if not isinstance(model, dict):
-					predictions.append(model)
+				# If the object is not a dictionary, return it as is, since we have reached a value
+				if not isinstance(root_node, dict):
+					predictions.append(root_node)
 					break
 				else:
-					node = next(iter(model))
-					value = data[node]
-					if value in model[node]:
-						model = model[node][value]
+					# Get the key for the dictionary of the node that we have reached
+					# This is the column index whose value we need to test
+					column_index = next(iter(root_node))
+
+					# Get the value from the column in the testing instance
+					feature_value = data[column_index]
+
+					# If the value is present as a key in the dictionary of the node, then move to that node
+					if feature_value in root_node[column_index]:
+						root_node = root_node[column_index][feature_value]
+					# Else, we have reached a value that we have not come across during the model building
+					# Abort the execution for the current data
 					else:
 						predictions.append(None)
 						break
